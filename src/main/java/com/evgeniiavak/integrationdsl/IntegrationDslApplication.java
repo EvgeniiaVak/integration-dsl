45be6603fe1f77182c4e1fb4f8d9d0342cab4813
vakarina.e <vakarina.e@cdek.ru>
2018-05-21 19:16:11 +0700

    simple sftp to ftp flow

--- src/main/java/com/evgeniiavak/integrationdsl/IntegrationDslApplication.java
+++ src/main/java/com/evgeniiavak/integrationdsl/IntegrationDslApplication.java
@@ -1,6 +1,8 @@
 package com.evgeniiavak.integrationdsl;
 
 import com.jcraft.jsch.ChannelSftp;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +13,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.core.Pollers;
-import org.springframework.integration.dsl.sftp.Sftp;
+import org.springframework.integration.dsl.ftp.Ftp;
 import org.springframework.integration.file.remote.session.CachingSessionFactory;
 import org.springframework.integration.file.remote.session.SessionFactory;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.file.transformer.FileToStringTransformer;
-import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
 import org.springframework.integration.metadata.SimpleMetadataStore;
 import org.springframework.integration.sftp.filters.SftpPersistentAcceptOnceFileListFilter;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 
 import java.io.File;
-import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 public class IntegrationDslApplication {
@@ -30,7 +30,10 @@ public class IntegrationDslApplication {
     private Logger logger = LoggerFactory.getLogger(IntegrationDslApplication.class);
 
     @Autowired
-    SessionFactory<ChannelSftp.LsEntry> sftpSessionFactory;
+    private SessionFactory<ChannelSftp.LsEntry> sftpSessionFactory;
+
+    @Autowired
+    private SessionFactory<FTPFile> ftpSessionFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(IntegrationDslApplication.class, args);
@@ -52,40 +55,41 @@ public class IntegrationDslApplication {
         return new CachingSessionFactory<>(factory);
     }
 
+    @Bean(name = "ftpSessionFactory")
+    public SessionFactory<FTPFile> ftpSessionFactory(
+            @Value(value = "${ftp.host:localhost}") String host,
+            @Value (value = "${ftp.port:21}") int port,
+            @Value (value = "${ftp.user:username}") String user,
+            @Value (value = "${ftp.password:12345678}") String password
+    ) {
+        DefaultFtpSessionFactory factory = new DefaultFtpSessionFactory();
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUsername(user);
+        factory.setPassword(password);
+        factory.setClientMode(FTPClient.PASSIVE_LOCAL_DATA_CONNECTION_MODE);
+        return new CachingSessionFactory<>(factory);
+    }
+
     @Bean
-    public IntegrationFlow sftpInboundFlow(@Value (value = "${sftp.directory:remote-dir}") String dir) {
+    public IntegrationFlow fromSftpToFtpFlow(@Value (value = "${sftp.directory:remote-dir}") String sftpDir,
+                                             @Value (value = "${ftp.directory:remote-dir}") String ftpDir) {
         return IntegrationFlows
                 .from(s -> s.sftp(sftpSessionFactory)
                                 .preserveTimestamp(true)
-                                .remoteDirectory(dir)
+                                .remoteDirectory(sftpDir)
                                 .regexFilter(".*\\.txt$")
-                                .deleteRemoteFiles(true)
+                                .filter(new SftpPersistentAcceptOnceFileListFilter(new SimpleMetadataStore(), "sftp-message"))
                                 .autoCreateLocalDirectory(true)
                                 .localDirectory(new File("tmp")),
                         e -> e.id("sftpInboundAdapter")
                                 .autoStartup(true)
                                 .poller(Pollers.fixedDelay(5000)))
-                .log(LoggingHandler.Level.INFO)
-                .<File,String> transform(new FileToStringTransformer())
-                .log(LoggingHandler.Level.INFO)
-                .handle(m -> System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Test results: \n" + m.getPayload()))
+                .log(message -> "\n\nTEST RESULTS (downloaded): \n" + message.getPayload())
+                .handle(Ftp.outboundAdapter(ftpSessionFactory, FileExistsMode.REPLACE)
+                                .autoCreateDirectory(true)
+                                .remoteDirectory(ftpDir))
                 .get();
     }
 
-    @Bean
-    public IntegrationFlow fileDeletionFlow(@Value (value = "${local.directory:tmp}") String localDir,
-                                            @Value (value = "${sftp.directory:remote-dir}") String remoteDir ) {
-        return IntegrationFlows
-                .from(s -> s.file(new File(localDir))
-                                .autoCreateDirectory(true)
-                                .regexFilter(".*\\.txt$"),
-                        e -> e.id("fileInboundAdapter")
-                                .autoStartup(true)
-                                .poller(Pollers.fixedDelay(5333)))
-                .log(LoggingHandler.Level.INFO)
-                .handle(Sftp.outboundAdapter(sftpSessionFactory, FileExistsMode.FAIL)
-                                .remoteDirectory(remoteDir)
-                                .autoCreateDirectory(true))
-                .get();
-    }
 }
